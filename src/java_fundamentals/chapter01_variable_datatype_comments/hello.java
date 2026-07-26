1a6e916800eaf11e162a42d5ae6ff57d8bfd2b5b
vishwanath7798 <vishwanathspatil07@gmail.com>
2026-07-26 13:40:38 +0530

    Added variables.java example

--- src/java_fundamentals/chapter01_variable_datatype_comments/hello.java
+++ src/java_fundamentals/chapter01_variable_datatype_comments/hello.java
@@ -1,7 +1,8 @@
-package java_fundamentals.chapter01_variable_datatype_comments.;
+package java_fundamentals.chapter01_variable_datatype_comments;
 
 public class hello {
     public static void main(String args[]){
         System.out.println("HELLO, WORLD!");
+        System.out.println(2+5);
     }
 }
---
+++ src/java_fundamentals/chapter01_variable_datatype_comments/variables.java
@@ -0,0 +1,12 @@
+package java_fundamentals.chapter01_variable_datatype_comments;
+
+public class variables {
+    public static void main(String args[]){
+        int age = 28;
+        int aGe = 11;
+        int marks = 98;
+//        int 2boy = 100; not allowed
+        int secondMarks = 23;
+//        int second marks of student = 23; not allowed
+    }
+}
