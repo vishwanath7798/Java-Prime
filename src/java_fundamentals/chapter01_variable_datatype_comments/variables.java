6a6e601de3257c7324cdb4e3bcbdf1b680ed1b26
vishwanath7798 <vishwanathspatil07@gmail.com>
2026-07-26 13:53:35 +0530

    add

--- src/java_fundamentals/chapter01_variable_datatype_comments/variables.java
+++ src/java_fundamentals/chapter01_variable_datatype_comments/variables.java
@@ -8,5 +8,6 @@ public class variables {
 //        int 2boy = 100; not allowed
         int secondMarks = 23;
 //        int second marks of student = 23; not allowed
+        System.out.println(secondMarks);
     }
 }
