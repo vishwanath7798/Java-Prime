782b7bc14140b5817159965927adee178944c45d
vishwanath7798 <vishwanathspatil07@gmail.com>
2026-07-27 19:16:50 +0530

    update

---
+++ src/java_fundamentals/chapter01_variable_datatype_comments/learnSwitch.java
@@ -0,0 +1,41 @@
+package java_fundamentals.chapter01_variable_datatype_comments;
+
+import java.util.Scanner;
+
+public class learnSwitch {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the day");
+        int day = sc.nextInt();
+
+//        if(day == 1) {
+//            System.out.println("Today is Sunday");
+//        } else if(day == 2) {
+//            System.out.println("Today is Sunday");
+//        }
+
+        switch (day) {
+            case 1: case 2:
+                System.out.println("Today is Sunday");
+                System.out.println("this is on sunday");
+                break;
+            case 3:
+                System.out.println("Today is Tuesday");
+                break;
+            case 4:
+                System.out.println("Today is Wednesday");
+                break;
+            case 5:
+                System.out.println("Today is Thursday");
+                break;
+            case 6:
+                System.out.println("Today is Friday");
+                break;
+            case 7:
+                System.out.println("Today is Saturday");
+                break;
+            default:
+                System.out.println("Invalid day");
+        }
+    }
+}
