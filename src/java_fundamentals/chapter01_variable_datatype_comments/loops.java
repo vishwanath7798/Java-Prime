695dff2608ce88666a21d1e08ec23bf9ba332f51
vishwanath7798 <vishwanathspatil07@gmail.com>
2026-07-27 20:54:19 +0530

    loops

---
+++ src/java_fundamentals/chapter01_variable_datatype_comments/loops.java
@@ -0,0 +1,14 @@
+package java_fundamentals.chapter01_variable_datatype_comments;
+
+import java.util.Scanner;
+
+public class loops {
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        int n = sc.nextInt();
+//        System.out.println("Hello World");
+        for (int i = 0; i < n; i++) {
+            System.out.println("Hello World");
+        }
+    }
+}
